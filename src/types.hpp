2f595064a5decca17f601042ee72b508d60acf8f
Bendik Samseth <b.samseth@gmail.com>
2018-02-14 18:19:52 +0100

    Add print functionality to Hole and Move structs.

--- src/types.hpp
+++ src/types.hpp
@@ -10,6 +10,9 @@
 #define types_h
 
 #include <array>
+#include <string>
+#include <sstream>
+
 
 enum Pebble {
     INVALID = 0, NO_PEBBLE, P1, P2, PEBBLE_NB
@@ -35,6 +38,8 @@ struct Move {
     Hole to;
 };
 
+constexpr Move NO_MOVE = {{0, 0}, {0, 0}};
+
 constexpr int X_SIZE = 19, Y_SIZE = 19;
 
 const std::array<Direction, 6> directions_steps =
@@ -47,5 +52,17 @@ const std::array<Direction, 6> directions_jumps =
     Direction{2,  0}, Direction{-2, 0}, Direction{0,  2},
     Direction{0, -2}, Direction{2, -2}, Direction{-2, 2}
 };
+
+
+
+inline std::ostream& operator<<(std::ostream &strm, const Hole &b) {
+    return strm << "(" << b.x << ", " << b.y << ")";
+}
+
+inline std::ostream& operator<<(std::ostream &strm, const Move &m) {
+    return strm << "[" << m.from << " -> " << m.to << "]";
+}
+
+
 
 #endif /* types_h */
