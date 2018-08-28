e9939eb2c4c29c237c8a05cea89b4c375a1c3411
Bendik Samseth <b.samseth@gmail.com>
2018-08-28 16:41:40 +0200

    Update .gitignore

--- build/CMakeFiles/gtest.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gtest"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
